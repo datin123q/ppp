41f04d979f8ca26acd3fe10c59e6daa58b257426
datin123q <82952938+datin123q@users.noreply.github.com>
2021-04-21 20:36:18 +0700

    Add files via upload

---
+++ shippp/src/application/BattleshipMain.java
@@ -0,0 +1,116 @@
+package application;
+
+import java.util.Random;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import application.Board.Cell;
+
+public class BattleshipMain extends Application {
+
+    private boolean running = false;
+    private Board enemyBoard, playerBoard;
+
+    private int shipsToPlace = 5;
+
+    private boolean enemyTurn = false;
+
+    private Random random = new Random();
+
+    private Parent createContent() {
+        BorderPane root = new BorderPane();
+        root.setPrefSize(1000, 600);
+
+        root.setRight(new Text("RIGHT SIDEBAR - CONTROLS"));
+
+        enemyBoard = new Board(true, event -> {
+            if (!running)
+                return;
+
+            Cell cell = (Cell) event.getSource();
+            if (cell.wasShot)
+                return;
+
+            enemyTurn = !cell.shoot();
+
+            if (enemyBoard.ships == 0) {
+                System.out.println("YOU WIN");
+            }
+
+            if (enemyTurn)
+                enemyMove();
+        });
+
+        playerBoard = new Board(false, event -> {
+            if (running)
+                return;
+
+            Cell cell = (Cell) event.getSource();
+            if (playerBoard.placeShip(new Ship(shipsToPlace, event.getButton() == MouseButton.SECONDARY), cell.x, cell.y)) {
+                if (--shipsToPlace == 0) {
+                    startGame();
+                }
+            }
+        });
+
+        HBox vbox = new HBox(50, enemyBoard, playerBoard);
+        vbox.setAlignment(Pos.CENTER);
+
+        root.setCenter(vbox);
+
+        return root;
+    }
+    private void enemyMove() {
+        while (enemyTurn) {
+            int x = random.nextInt(10);
+            int y = random.nextInt(10);
+            Cell cell = playerBoard.getCell(x, y);
+            if (cell.wasShot) 
+                continue;
+            enemyTurn = cell.shoot();
+            if (playerBoard.ships == 0) {
+                System.out.println("YOU LOSE");
+            }
+        }
+    }
+
+    private void startGame() {
+        // place enemy ships
+        int type = 5;
+
+        while (type > 0) {
+            int x = random.nextInt(10);
+            int y = random.nextInt(10);
+
+            if (enemyBoard.placeShip(new Ship(type, Math.random() < 0.5), x, y)) {
+                type--;
+            }
+        }
+
+        running = true;
+    }
+
+    @Override
+    public void start(Stage primaryStage)  {
+        Scene scene = new Scene(createContent());
+        primaryStage.setTitle("Battleship");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public void run( Stage primaryStage) {
+
+    	createContent();
+    	enemyMove() ;
+     	start(primaryStage);
+    }
+}
